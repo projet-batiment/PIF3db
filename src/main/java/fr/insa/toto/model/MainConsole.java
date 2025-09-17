3554e4c6a710ca0bf960c71a2ecddc49d56791f1
insastrasbge1 <insastrasbge1@gmail.com>
2025-09-17 22:37:17 +0200

    menusTexte

---
+++ src/main/java/fr/insa/toto/model/MainConsole.java
@@ -0,0 +1,114 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package fr.insa.toto.model;
+
+import fr.insa.beuvron.utils.ConsoleFdB;
+import fr.insa.beuvron.utils.database.ConnectionSimpleSGBD;
+import fr.insa.beuvron.utils.database.ResultSetUtils;
+import fr.insa.beuvron.utils.exceptions.ExceptionsUtils;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author francois
+ */
+public class MainConsole {
+
+    public static void menuUtilisateur(Connection con) {
+        int rep = -1;
+        while (rep != 0) {
+            int i = 1;
+            System.out.println("Menu utilisateurs");
+            System.out.println("============================");
+            System.out.println((i++) + ") liste des utilisateurs");
+            System.out.println((i++) + ") autre");
+            System.out.println("0) Retour");
+            rep = ConsoleFdB.entreeEntier("Votre choix : ");
+            try {
+                int j = 1;
+                if (rep == j++) {
+                    System.out.println("TODO");
+                } else if (rep == j++) {
+                    System.out.println("TODO autres");
+                }
+            } catch (Exception ex) {
+                System.out.println(ExceptionsUtils.messageEtPremiersAppelsDansPackage(ex, "fr.insa", 3));
+            }
+        }
+    }
+
+    public static void menuBdD(Connection con) {
+        int rep = -1;
+        while (rep != 0) {
+            int i = 1;
+            System.out.println("Menu gestion base de données");
+            System.out.println("============================");
+            System.out.println((i++) + ") RAZ BdD = delete + create + init");
+            System.out.println((i++) + ") donner un ordre SQL update quelconque");
+            System.out.println((i++) + ") donner un ordre SQL query quelconque");
+            System.out.println("0) Retour");
+            rep = ConsoleFdB.entreeEntier("Votre choix : ");
+            try {
+                int j = 1;
+                if (rep == j++) {
+                    GestionSchema.razBdd(con);
+                    BdDTest.createBdDTestV1(con);
+                } else if (rep == j++) {
+                    String ordre = ConsoleFdB.entreeString("ordre SQL : ");
+                    try (PreparedStatement pst = con.prepareStatement(ordre)) {
+                        pst.executeUpdate();
+                    }
+                } else if (rep == j++) {
+                    String ordre = ConsoleFdB.entreeString("requete SQL : ");
+                    try (PreparedStatement pst = con.prepareStatement(ordre)) {
+                        try (ResultSet rst = pst.executeQuery()) {
+                            System.out.println(ResultSetUtils.formatResultSetAsTxt(rst));
+                        }
+                    }
+                }
+            } catch (Exception ex) {
+                System.out.println(ExceptionsUtils.messageEtPremiersAppelsDansPackage(ex, "fr.insa", 3));
+            }
+        }
+    }
+
+    public static void menuPrincipal() {
+        int rep = -1;
+        Connection con = null;
+        try {
+            con = ConnectionSimpleSGBD.defaultCon();
+            System.out.println("Connection OK");
+        } catch (SQLException ex) {
+            System.out.println("Problème de connection : " + ex.getLocalizedMessage());
+            throw new Error(ex);
+        }
+        while (rep != 0) {
+            int i = 1;
+            System.out.println("Menu principal");
+            System.out.println("==================");
+            System.out.println((i++) + ") menu gestion BdD");
+            System.out.println((i++) + ") menu utilisateurs");
+            System.out.println("0) Fin");
+            rep = ConsoleFdB.entreeEntier("Votre choix : ");
+            try {
+                int j = 1;
+                if (rep == j++) {
+                    menuBdD(con);
+                } else if (rep == j++) {
+                    menuUtilisateur(con);
+                }
+            } catch (Exception ex) {
+                System.out.println(ExceptionsUtils.messageEtPremiersAppelsDansPackage(ex, "fr.insa", 3));
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        menuPrincipal();
+    }
+}

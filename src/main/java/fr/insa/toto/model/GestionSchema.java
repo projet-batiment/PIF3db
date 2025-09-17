5e8e733709a86e25f2383520af8dc6b64a5291ec
insastrasbge1 <insastrasbge1@gmail.com>
2025-09-17 21:10:05 +0200

    onlySchema

---
+++ src/main/java/fr/insa/toto/model/GestionSchema.java
@@ -0,0 +1,90 @@
+/*
+Copyright 2000- Francois de Bertrand de Beuvron
+
+This file is ecole of CoursBeuvron.
+
+CoursBeuvron is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+CoursBeuvron is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with CoursBeuvron.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package fr.insa.toto.model;
+
+import fr.insa.beuvron.utils.database.ConnectionSimpleSGBD;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.sql.Time;
+import java.time.LocalTime;
+
+/**
+ *
+ * @author francois
+ */
+public class GestionSchema {
+
+    /**
+     *
+     * @param con
+     * @throws SQLException
+     */
+    public static void creeSchema(Connection con)
+            throws SQLException {
+        try (Statement st = con.createStatement()) {
+            // creation des tables
+            st.executeUpdate("create table utilisateur ( "
+                    + " id integer not null primary key, "
+                    + " surnom varchar(30) not null unique,"
+                    + " pass varchar(20)  "
+            );
+        }
+    }
+
+    /**
+     *
+     * @param con
+     * @throws SQLException
+     */
+    public static void deleteSchema(Connection con) throws SQLException {
+        try (Statement st = con.createStatement()) {
+            try {
+                st.executeUpdate("drop table user");
+            } catch (SQLException ex) {
+            }
+        }
+    }
+
+    /**
+     *
+     * @param con
+     * @throws SQLException
+     */
+    public static void razBdd(Connection con) throws SQLException {
+        deleteSchema(con);
+        creeSchema(con);
+    }
+
+    /**
+     *
+     * @param args
+     */
+    public static void main(String[] args) {
+        try (Connection con = ConnectionSimpleSGBD.defaultCon()) {
+            razBdd(con);
+        } catch (SQLException ex) {
+            throw new Error(ex);
+        }
+    }
+
+}
